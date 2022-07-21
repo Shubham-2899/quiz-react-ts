402215bb16f5262b735489840ce7b1bcffe744c0
Shubham-2899 <shubhamlatake123@gmail.com>
2022-07-21 11:25:50 +0530

    Learning git

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
-import './App.css';
-import { Quiz } from './Quiz';
+import React from "react";
+import "./App.css";
+import { Quiz } from "./Quiz";
 
 function App() {
   return (
     <div className="App">
+      <h1>Inside the learning branch</h1>
       <Quiz />
     </div>
   );
